6dc02e5086021efa781f3f46b97a61d2763852f5
siwacarn <siwacarn.sir@gmail.com>
2019-11-18 14:49:41 +0700

    update complete

--- app/app.go
+++ app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"api/config"
+	"api/handler"
 	"api/model"
 	"fmt"
 	"log"
@@ -70,4 +71,35 @@ func (a *App) Delete(path string, f func(w http.ResponseWriter, r *http.Request)
 }
 
 // Handlers to manage Employee Data
-//to be continue
+func (a *App) GetAllEmployees(w http.ResponseWriter, r *http.Request) {
+	handler.GetAllEmployees(a.DB, w, r)
+}
+
+func (a *App) CreateEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.CreateEmployee(a.DB, w, r)
+}
+
+func (a *App) GetEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.GetEmployee(a.DB, w, r)
+}
+
+func (a *App) UpdateEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.UpdateEmployee(a.DB, w, r)
+}
+
+func (a *App) DeleteEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.DeleteEmployee(a.DB, w, r)
+}
+
+func (a *App) DisableEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.DisableEmployee(a.DB, w, r)
+}
+
+func (a *App) EnableEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.DisableEmployee(a.DB, w, r)
+}
+
+//]run the app on it's router
+func (a *App) Run(host string) {
+	log.Fatal(http.ListenAndServe(host, a.Router))
+}
